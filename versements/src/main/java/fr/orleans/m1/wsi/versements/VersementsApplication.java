5a3a896cfe0b7c8d6967e0060f9473dd726291a4
Luffy-hash <ba654abdoulaye@gmail.com>
2024-08-25 06:35:58 +0200

    init backend project

---
+++ versements/src/main/java/fr/orleans/m1/wsi/versements/VersementsApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wsi.versements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersementsApplication.class, args);
+    }
+
+}
